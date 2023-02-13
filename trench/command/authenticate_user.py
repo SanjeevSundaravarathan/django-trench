af255f66649c15a62438ba24877fecbd6087bcaf
Sanjeev Sundaravarathan <sanjeev@mosaic.ie>
2023-02-13 16:17:57 +0000

    authenticate user

--- trench/command/authenticate_user.py
+++ trench/command/authenticate_user.py
@@ -12,6 +12,12 @@ User: AbstractUser = get_user_model()
 class AuthenticateUserCommand:
     @staticmethod
     def execute(request: Request, username: str, password: str) -> User:
+        try:
+            user_with_matching_email = User.objects.get(email__iexact=username)
+            username = user_with_matching_email.email
+        except User.DoesNotExist:
+            raise UnauthenticatedError()
+
         user = authenticate(
             request=request,
             username=username,
